9b0c3dc612f033c3bf8b5d369be91061ced36448
MrClaro <adryan.contatoprofissional@gmail.com>
2026-02-25 10:53:15 -0300

    chore(eslint): configura prettier, unused-imports e simple-import-sort
    Configura o ESLint Flat Config para:
    - Formatar código automaticamente com Prettier
    - Remover imports não utilizados automaticamente
    - Ordenar e agrupar imports por ordem alfabética
    - Corrigir o parsing de templates Angular no Prettier

--- athena/src/app/integrador/integrador-module.ts
+++ athena/src/app/integrador/integrador-module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { IntegradorRoutingModule } from './integrador-routing-module';
 
-
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    IntegradorRoutingModule
-  ]
+  imports: [CommonModule, IntegradorRoutingModule],
 })
-export class IntegradorModule { }
+export class IntegradorModule {}

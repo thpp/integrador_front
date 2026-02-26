f37463c639e48d8a98c5050df2ed25edeaca65cd
MrClaro <adryan.contatoprofissional@gmail.com>
2026-02-26 16:31:45 -0300

    feat(module): configura modulo integrador para permitir acesso à rota de projetos

--- athena/src/app/integrador/integrador-module.ts
+++ athena/src/app/integrador/integrador-module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { IntegradorRoutingModule } from './integrador-routing-module';
+import { ProjetoList } from './pages/projetos/projeto-list/projeto-list';
+import { ProjetoForm } from './pages/projetos/projeto-form/projeto-form';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, IntegradorRoutingModule],
+  declarations: [ProjetoForm],
+  imports: [CommonModule, IntegradorRoutingModule, ProjetoList],
 })
 export class IntegradorModule {}
--- athena/src/app/integrador/integrador-routing-module.ts
+++ athena/src/app/integrador/integrador-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjetoList } from './pages/projetos/projeto-list/projeto-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projetos',
+    component: ProjetoList,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

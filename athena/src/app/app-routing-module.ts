8b20f22ae5ade546a7257f2b772b1fd77199bfac
MrClaro <adryan.contatoprofissional@gmail.com>
2026-02-26 16:32:38 -0300

    feat(app): configura arquivo de routing principal para permitir acesso as rotas do modulo integrador
    - Configura página principal para receber o router outlet

--- athena/src/app/app-routing-module.ts
+++ athena/src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'integrador',
+    loadChildren: () => import('./integrador/integrador-module').then((m) => m.IntegradorModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
